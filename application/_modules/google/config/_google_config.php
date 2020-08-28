766237b91d93351501309cdf40f518bff4fc1a87
roniwahyu <roniwahyu@gmail.com>
2020-08-28 14:00:13 +0700

    delete unused obsolete modules

--- application/_modules/google/config/_google_config.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['google_client_id']="480061210784-vtikld7btq04f8r9v9aecjtkp00juq66.apps.googleusercontent.com";
-$config['google_client_secret']="zO16KGEckQn828lIO6FzFCXp";
-$config['google_redirect_url']=base_url().'auth/oauth2callback';
-
--- application/_modules/membersx/views/site.php
+++
@@ -1,8 +0,0 @@
-<?php //getcomponent('wizard5') 
-if(!empty($jurusan)){
-	print_r($jurusan);
-}
-foreach ($data as $key => $value) {
-	echo "<h1>".$value['jurusan']."</h1>";
-};
-?>
